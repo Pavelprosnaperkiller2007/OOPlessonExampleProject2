a575fc7e28099276865215505a1016cdd0bacb43
Vladimr Kondratsenko <vovanych1981@gmail.com>
2023-04-27 18:42:42 +0300

    aff

--- OPlessonExampleProject/manager.cpp
+++ OPlessonExampleProject/manager.cpp
@@ -1,1 +1,10 @@
+#pragma  once
 #include "manager.h"
+
+class Manager {
+public:
+	float calStudentsAvgAge(Student* list, int size);
+	float calStudentsAvgAge(Student* list, int size);
+	Student findBestStudent(Student* list, int size);
+	Student findWorstStudent(Student* list, int size);
+};
