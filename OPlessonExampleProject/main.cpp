41cd98939157927ea5798acecbc294f4163edec4
Vladimr Kondratsenko <vovanych1981@gmail.com>
2023-05-02 18:33:07 +0300

    aqs

--- OPlessonExampleProject/main.cpp
+++ OPlessonExampleProject/main.cpp
@@ -1,7 +1,15 @@
 #include "student.h"
 
 int main() {
-	Student st;
+	
+		Student s1("Ivan", "Ivanov", 14, 7.6);
+	Student s2("Alex", "Invisible", 15, 10);
+	Student s3("Peter", "Pen", 12, 4);
+	Student s4("Harry", "Potter", 14, 9.6);
+	Student s5("Alice", "Inder", 13, 8);
+	
+	
+
 
 	return 0;
 }
--- OPlessonExampleProject/manager.cpp
+++ OPlessonExampleProject/manager.cpp
@@ -4,7 +4,10 @@
 class Manager {
 public:
 	float calStudentsAvgAge(Student* list, int size);
+
 	float calStudentsAvgAge(Student* list, int size);
+
 	Student findBestStudent(Student* list, int size);
+
 	Student findWorstStudent(Student* list, int size);
 };
